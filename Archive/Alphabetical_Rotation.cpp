db2e862250e8e600fe74e6fa3e0dddf6378e34df
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-03-07 00:40:53 +0530

    Create Alphabetical_Rotation.cpp

---
+++ Archive/Alphabetical_Rotation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  const int lower = 155;
+  const int upper = 219;
+	string name;
+	
+	cin >> name;
+
+	for(char c : name)
+	{
+	    (c < 97) ? cout << char(lower - c) : cout << char(upper - c);
+	}
+	
+	return 0;
+}
