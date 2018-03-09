a6bae0cb504cbb55424b9381385b8cc9164eead7
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-03-09 18:12:16 +0530

    Update and rename Sieve.c to Sieve.cpp

---
+++ Archive/Sieve.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+#define prime true
+#define non_prime false
+using namespace std;
+
+main()
+{
+	int T;
+
+		cin >> T;
+
+		while(T)
+		{
+			int m, n;
+
+				cin >> m >> n;
+
+			bool input[n+1]{prime};
+
+				input[0] = input[1] = {non_prime};
+
+				for(int i = 2; i <= sqrt(n); i++)
+					if(input[i])
+						for(int j = 2; i*j <= n; j++)
+							input[i*j] = {non_prime};
+
+				for(int i = m; i <=n; i++)
+					if(input[i] == prime)
+						cout << i << endl;
+
+				puts("");
+		}
+
+	return 0;
+}
+
+/*
+This code is obscure and only work as logic.
