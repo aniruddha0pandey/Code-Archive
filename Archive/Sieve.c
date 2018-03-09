a2381c758ab3369e6cded7040837232a64be9ef0
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-03-09 17:40:38 +0530

    Update Sieve.c

--- Archive/Sieve.c
+++ Archive/Sieve.c
@@ -4,14 +4,14 @@
 #include <stdbool.h>
 #define limit n-m+1
      
-main(){
-     
+main()
+{
     int T,i;
      
     	scanf("%d", &T);
      
-    	for(i = 0; i < T; i++){
-     
+    	for(i = 0; i < T; i++)
+     {
     		int m, n;
      
     			scanf("%lld%lld", &m, &n);
@@ -19,20 +19,20 @@ main(){
     		int j;
     		bool input[limit];
      
-    			for(j = m; j <= n; j++)			  //marking every number as prime
+    			for(j = m; j <= n; j++)			       //marking every number as prime
     				input[j] = true;
      
-    			input[0] = input[1] = false;	  //marking exceptions as non-prime
+    			input[0] = input[1] = false;	            //marking exceptions as non-prime
      
-    	 	int k, sqr = sqrt(n);				  //functino "sqrt()" will return float
+    	 	int k, sqr = sqrt(n);				       //functino "sqrt()" will return float
      
     			for(j = 2; j <= sqr; j++)
-    				if(input[j])			  //selecting only primes
+    				if(input[j])			            //selecting only primes
     					for(k = 2; (j * k) <= n; k++)   //marking multiples of 2,3,4...so on upto root of n
-    						input[k * j] = false; //marking non-primes
+    						input[k * j] = false;      //marking non-primes
      
     			for(j = m; j <= n; j++)
-    				if(input[j] == true)		  //displaying all primes
+    				if(input[j] == true)		       //displaying all primes
     					printf("%d\n", j);
      
     			puts("");
