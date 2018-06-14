b08a7da9bdd94d52688981ae9c44426510d7ae9f
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-06-15 05:14:24 +0530

    Update lexicographic.cpp

--- Archive/Permutation/lexicographic.cpp
+++ Archive/Permutation/lexicographic.cpp
@@ -2,20 +2,20 @@
 #include <algorithm>
 
 // i.e arr[i - 1] is pivot
-inline bool iHeadIndex(int arr[], int i) { return i > 0 && arr[i - 1] >= arr[i]; }
+inline bool iHeadIndex(int arr[], std::size_t i) { return i > 0 && arr[i - 1] >= arr[i]; }
 
 // i.e arr[j] new pivot
-inline bool jHeadIndex(int arr[], int i, int j) { return arr[j] <= arr[i - 1]; }
+inline bool jHeadIndex(int arr[], std::size_t i, std::size_t j) { return arr[j] <= arr[i - 1]; }
 
-char print(int arr[], int size) {
+char print(int arr[], std::size_t size) {
 	for (int i = 0; i < size; i++)
 		std::cout << "|" << arr[i] << "|";
 	return puts("");
 }
 
-bool permute(int arr[], int size) {
+bool permute(int arr[], std::size_t size) {
 	// longest non-increasing suffix
-	int i = size - 1;
+	std::size_t i = size - 1;
 	while (iHeadIndex(arr, i))
 		i--;
 
@@ -24,7 +24,7 @@ bool permute(int arr[], int size) {
 		return false;
 
 	// rightmost element greater than pivot
-	int j = size - 1;
+	std::size_t j = size - 1;
 	while (jHeadIndex(arr, i, j))
 		j--;
 
@@ -39,7 +39,7 @@ bool permute(int arr[], int size) {
 
 int main () {
 	int arr[] = {1, 2, 3};
-	size_t size = sizeof(arr) / sizeof(int);
+	std::size_t size = sizeof(arr) / sizeof(int);
 
 	do {
 		print(arr, size);
