14273e0b4db0a05e9e1a295c4b04a7ddebd12552
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-06-19 18:33:27 +0530

    Update String_Permutation.c

--- Archive/String_Permutation.c
+++ Archive/String_Permutation.c
@@ -5,24 +5,24 @@ swap(char *left_pointer, char *right_pointer)
 {
 	char temp;
 	
-		temp = *left_pointer;
-		*left_pointer = *right_pointer;
-		*right_pointer = temp;
+	temp = *left_pointer;
+	*left_pointer = *right_pointer;
+	*right_pointer = temp;
 }
 
 permute(char *pointer, int left, int right)
 {
 	int i;
-		
-		if(left == right)
-			printf("%s\n", pointer);
-		else
-			for(i = left; i <= right; i++)
-			{
-				swap((pointer + left),(pointer + i));
-				permute(pointer, left + 1, right);
-				swap((pointer + left), (pointer + i)); //backtrack	
-			}
+
+	if(left == right)
+		printf("%s\n", pointer);
+	else
+		for(i = left; i <= right; i++)
+		{
+			swap((pointer + left),(pointer + i));
+			permute(pointer, left + 1, right);
+			swap((pointer + left), (pointer + i)); //backtrack	
+		}
 }
 
 main(){
@@ -30,7 +30,7 @@ main(){
 	char string[] = "ABC";
 	int n = strlen(string);
   
-		permute(string, 0, n-1);
+	permute(string, 0, n-1);
 
 	return 0;
 
