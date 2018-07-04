66f0d42baf929743d810ffdf065511024779cb06
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-07-05 04:04:17 +0530

    Update contagiousPowerSet.c

--- Archive/Subset/contagiousPowerSet.c
+++ Archive/Subset/contagiousPowerSet.c
@@ -1,49 +1,48 @@
 #include <stdio.h>
  
-void subset(int arr[],int data[],int start,int end,int index,int r){
+void subset(int arr[],int data[],int start,int end,int index,int r) {
     
 	int j,i;
 		
-	    if(index==r){
+	if (index==r) {
 	    	
-			    for (j=0;j<r;j++)
-			        printf("%d ",data[j]);
+		for (j=0;j<r;j++)
+			printf("%d ",data[j]);
 			            
-				printf("\n");
+		printf("\n");
 	        
-	        return;
-	        
-	    }for(i=start;i<=end&&end-i+1>=r-index;i++){
+	    return;
+	}
+
+	for (i=start;i<=end&&end-i+1>=r-index;i++) {
 	    	
-	        data[index]=arr[i];
-	        subset(arr,data,i+1,end,index+1,r);
+	    data[index]=arr[i];
+	    subset(arr,data,i+1,end,index+1,r);
 	        
-	    }
+	}
     
 }
  
-void printsubset(int arr[],int n,int r){
+void printsubset(int arr[],int n,int r) {
     
 	int data[r];
 	
-    	subset(arr,data,0,n-1,0,r);
+    subset(arr,data,0,n-1,0,r);
     	
 }
 
-int main(){
-
+int main() {
 	int arr[20],k,n,i;
 	
-	    printf("Enter the number of input: ");
-	    scanf("%d",&n);
-	    printf("\nEnter the integers:\n");
+	printf("Enter the number of input: ");
+	scanf("%d",&n);
+	printf("\nEnter the integers:\n");
 	    
-	    for(i=0;i<n;i++)
-	    	scanf("%d",&arr[i]);
+	for(i=0;i<n;i++)
+		scanf("%d",&arr[i]);
 	    
-	    for(k=0;k<n;k++)
-	    	printsubset(arr,n,k);
+	for(k=0;k<n;k++)
+		printsubset(arr,n,k);
 
     return 0;
-    
 }
