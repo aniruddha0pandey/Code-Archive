10015928877901f8ce6d0ed46df951088237a724
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-07-05 04:01:26 +0530

    Update bitwise.cpp

--- Archive/Subset/bitwise.cpp
+++ Archive/Subset/bitwise.cpp
@@ -1,1 +1,18 @@
+// -std=c++1y
+#include <iostream>
 
+void subset(char arr[], int n) {
+    for(int i = 0; i < (1 << n); i++) {
+        for(int j = 0; j < n; j++)
+            if(i & (1 << j))
+                std::cout << arr[j] << " ";
+        puts("");
+    }
+}
+
+int main (void) {
+    char arr[] = {'a', 'b', 'c'};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    subset(arr, n);
+    return 0;
+}
