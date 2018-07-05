a8a3635234ef92e406c9f45d23447f6e545c2b9b
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-07-05 21:53:10 +0530

    Update aniGraphics.cpp

--- Archive/aniGraphics.cpp
+++ Archive/aniGraphics.cpp
@@ -9,48 +9,45 @@ const char bottomRight = 188;
 
 void top(int width) {
 	std::cout << topLeft;
-
 	for (int i = 0; i < width; i++)
 		std::cout << horizontal;
-
 	std::cout << topRight << std::endl;
 }
 
 void middle(int height, int width) {
 	for (int i = 0; i < height; i++) {
 		std::cout << vertical;
-
 		for (int j = 0; j < width; j++)
 			std::cout << " ";
-
 		std::cout << vertical << std::endl;		
 	}
 }
 
 void bottom(int width) {
 	std::cout << bottomLeft;
-
 	for (int i = 0; i < width; i++)
 		std::cout << horizontal;
-
 	std::cout << bottomRight << std::endl;
 }
 
 void box(int height, int width) {
-	top(width);
-	middle(height, width);
-	bottom(width);
+	if (height <= 0 && width <= 0) {
+		std::cout << "ERROR";
+	} else {
+		top(width);
+		if (height != 1 && width != 1)
+			middle(height, width);
+		bottom(width);	
+	}
 }
 
 int main () {
 	int height, width;
-
 	std::cout << "Enter Height: ";
 	std::cin >> height;
 	std::cout << "Enter Width: ";
 	std::cin >> width;
 	puts("");
-
 	box(height, width);
 	return 0;
 }
