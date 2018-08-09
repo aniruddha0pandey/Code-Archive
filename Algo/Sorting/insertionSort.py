6df096a128e2a55458728ce3d77942e11887b260
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-10 01:16:03 +0530

    Update insertionSort.py

--- Algo/Sorting/insertionSort.py
+++ Algo/Sorting/insertionSort.py
@@ -7,4 +7,11 @@ Maintenance    :
 Termination    :
 """
 
-  
+list = [int(i) for i in input("Input Elements: ").split()]
+for j = 2 to len(list)
+  key = list[j]
+  i = j - 1
+  while i > 0 and list[i] > key
+    list[i + 1] = list[i]
+    i--
+  list[i +1] = key
