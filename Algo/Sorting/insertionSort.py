6af2e8d8aa4a4e6199ed25a74b393cbbde33631c
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-10 01:07:24 +0530

    Update and rename insertionSort.cpp to insertionSort.py

---
+++ Algo/Sorting/insertionSort.py
@@ -0,0 +1,10 @@
+"""
+Input          : A sequence of n numbers ⟨a₁, a₂, a₃, ..., aₙ⟩ or a[1...n].
+Output         : A permutation (reordering) ⟨a₁, a₂, a₃, ..., aₙ⟩ of the input sequence such that {a₁' ≤ a₂' ≤ a₃' ≤ ... ≤ aₙ'}.
+Explanation    : The elements are also called as keys.
+Initialization :
+Maintenance    :
+Termination    :
+"""
+
+  
