c512fedba507df5d4cb54c7f2475cb7f2dbb5003
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-09 23:07:17 +0530

    Update insertionSort.cpp

--- Algo/Sorting/insertionSort.cpp
+++ Algo/Sorting/insertionSort.cpp
@@ -1,2 +1,2 @@
-Input:  A sequence of n numbers ⟨a₁, a₂, a₃, ..., aₙ⟩.
-Output: A permutation (reordering) ⟨a₁, a₂, a₃, ..., aₙ⟩ of the input sequence such that a₁ᶥ ≤ a₂ᶥ ≤ a₃ᶥ ≤ ... ≤ aₙᶥ.
+// Input:  A sequence of n numbers ⟨a₁, a₂, a₃, ..., aₙ⟩.
+// Output: A permutation (reordering) ⟨a₁, a₂, a₃, ..., aₙ⟩ of the input sequence such that a₁' ≤ a₂' ≤ a₃' ≤ ... ≤ aₙ'.
