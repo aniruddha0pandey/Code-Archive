d3c3038b818cea2c6747a82d45617620f7073927
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-10 15:10:10 +0530

    Update insertionSort.py

--- Algo/Sorting/insertionSort.py
+++ Algo/Sorting/insertionSort.py
@@ -1,15 +1,14 @@
 """
 Input          : A sequence of n numbers ⟨a₁, a₂, a₃, ..., aₙ⟩ or a[1...n].
 Output         : A permutation (reordering) ⟨a₁, a₂, a₃, ..., aₙ⟩ of the input sequence such that {a₁' ≤ a₂' ≤ a₃' ≤ ... ≤ aₙ'}.
-Explanation    : The elements are also called as keys.
-Initialization :
+Initialization : 
 Maintenance    :
 Termination    :
 """
 
 def insertionSort(list):
-    for j = 2 to len(list):
-        key = list[j]
+    for j = 1 to len(list):                 # j   : "current key".
+        key = list[j]                       # key : "elements also called keys".
         i = j - 1
         while i > 0 and list[i] > key:
             list[i + 1] = list[i]
