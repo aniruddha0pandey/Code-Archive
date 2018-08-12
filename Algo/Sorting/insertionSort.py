9d76ac2ebc89973bba5b29649c8d6a5428c0e84b
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-13 02:27:49 +0530

    Update insertionSort.py

--- Algo/Sorting/insertionSort.py
+++ Algo/Sorting/insertionSort.py
@@ -1,9 +1,9 @@
 """ docstring
 Input          : A sequence of n numbers ⟨a₁, a₂, a₃, ..., aₙ⟩ or a[1 ... n].
 Output         : A permutation (reordering) ⟨a₁, a₂, a₃, ..., aₙ⟩ of the input sequence such that {a₁' ≤ a₂' ≤ a₃' ≤ ... ≤ aₙ'}.
-Initialization : The 'loop invariant' holds before the first loop iteration:
+Initialization : The 'loop invariant' holds before the first loop iteration -
                  The subarray a[1 ... j-1] therefore consists of a[0]. Moreover, this subarray is sorted.
-Maintenance    : Each iteration maintain the 'loop invariant':
+Maintenance    : Each iteration maintain the 'loop invariant' -
                  Incrementing 'j' for the next iteration preserves 'loop invriant' in a[1 ... j-1].
 Termination    : Observing the subarray a[1 ... j-1] is infact the entire array, we conclude the entire array sorted.
 Analysis       : 
