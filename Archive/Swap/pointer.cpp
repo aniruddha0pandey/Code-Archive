4e6f3bf6eea825342c1c215c807f878296af725c
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-13 03:32:49 +0530

    Update pointer.cpp

--- Archive/Swap/pointer.cpp
+++ Archive/Swap/pointer.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 
 void swap (int* a, int* b, int tmp) {
   tmp = *a;
@@ -7,8 +8,11 @@ void swap (int* a, int* b, int tmp) {
 }
 
 int main (void) {
+  double t = clock();
   int x, y;
   std::cin >> x >> y;
   swap(&x, &y);
+  std::cout << "Execution Time   : " << (clock() - t) / CLOCKS_PER_SEC / 100.0 << std::endl;
+  std::cout << "Process Returned : " << "0 (0x0)" << std::endl;
   return 0;
 }
