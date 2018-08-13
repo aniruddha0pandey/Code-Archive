721ffc207106ce44a395c27daf1a464729f902da
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-13 06:50:14 +0530

    Update mergeSort.py

--- Algo/Sorting/mergeSort.py
+++ Algo/Sorting/mergeSort.py
@@ -5,5 +5,5 @@ def divide(list):
 def mergeSort(list):
     if (len(list) <= 1):
         return arr
-    left, right = half(arr)
+    left, right = half(list)
     return merge(mergeSort(left), mergeSort(right))
