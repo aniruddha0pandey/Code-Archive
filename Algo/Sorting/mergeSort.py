7a77b5dcb15e79d3a30bfba4b5519f10eb0727a0
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-13 06:56:32 +0530

    Update mergeSort.py

--- Algo/Sorting/mergeSort.py
+++ Algo/Sorting/mergeSort.py
@@ -1,9 +1,30 @@
+def merge(left, right):
+    out = []
+    while (len(left) > 0 and len(right) > 0): 
+        if (left[0] <= right[0]):
+            out.append(left[0])
+            del left[0]
+        else:
+            out.append(right[0])
+            del right[0]
+    while (len(left) > 0):
+        out.append(left[0])
+        del left[0]
+    while (len(right) > 0):
+        out.append.right[0])
+        del right[0]
+    return out
+
 def divide(list):
     mid = len(arr) / 2
     return arr[:mid], arr[mid:]
-    
+
 def mergeSort(list):
     if (len(list) <= 1):
         return list
     left, right = divide(list)
     return merge(mergeSort(left), mergeSort(right))
+
+if __name__ == "__main__":
+    list = [int(i) for i in input("Input Elements: ").split()]
+    print(mergeSort(list))
