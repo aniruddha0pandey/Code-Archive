fc6d2b2c1cb779c73e04723b04b02b0fae4ebf3b
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-17 16:33:21 +0530

    Create aniTemplate.cpp

---
+++ Archive/aniTemplate.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ull unsigned long long
+
+template<class T> inline T gcd(T x, T y) { if (!y) return x; return gcd(y, x%y);}
+
+const int MOD = 1e9 + 7;
+const double low = -1e10;
+const double high = 1e10;
+const long long inf = (long long) 1e18;
