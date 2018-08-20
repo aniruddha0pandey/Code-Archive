8d8d67dce7acf6d7527212f6d6b2038b49a468f4
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-20 19:33:00 +0530

    Update aniTemplate.cpp

--- Archive/aniTemplate.cpp
+++ Archive/aniTemplate.cpp
@@ -9,7 +9,7 @@ const double high = 1e10;
 const long long inf = (long long) 1e18;
 
 template<class T> inline T gcd(T x, T y) { if(!y) return x; return gcd(y, x%y); }
-inline void boost(void) { std::ios::sync_with_stdio(false); std::cin.tie(0); }
+inline void boost(void) { std::ios::sync_with_stdio(false); std::cin.tie(NULL); }
 
 int main (void) {
   boost();
