af6df6512c78228d7e0d369b2f6f50b448dc8503
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-20 18:55:24 +0530

    Update aniTemplate.cpp

--- Archive/aniTemplate.cpp
+++ Archive/aniTemplate.cpp
@@ -3,6 +3,8 @@
 #define ll long long
 #define ull unsigned long long
 
+#define boost ios::sync_with_stdio(false);cin.tie(0); 
+
 template<class T> inline T gcd(T x, T y) { if (!y) return x; return gcd(y, x%y);}
 
 const int MOD = 1e9 + 7;
@@ -11,6 +13,7 @@ const double high = 1e10;
 const long long inf = (long long) 1e18;
 
 int main (void) {
+  boost;
   // Seed the random-number generator with current time so that
   // the numbers will be different every time we run <rand()>.
   srand( (unsigned)time( NULL ) );
