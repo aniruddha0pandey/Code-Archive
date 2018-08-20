cbaa714f805e54238d5fb5da856284b32e2fac05
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-20 19:32:24 +0530

    Update aniTemplate.cpp

--- Archive/aniTemplate.cpp
+++ Archive/aniTemplate.cpp
@@ -3,13 +3,13 @@
 #define ll long long
 #define ull unsigned long long
 
-template<class T> inline T gcd(T x, T y) { if(!y) return x; return gcd(y, x%y); }
-inline void boost(void) { std::ios::sync_with_stdio(false); std::cin.tie(0); }
-
 const int MOD = 1e9 + 7;
 const double low = -1e10;
 const double high = 1e10;
 const long long inf = (long long) 1e18;
+
+template<class T> inline T gcd(T x, T y) { if(!y) return x; return gcd(y, x%y); }
+inline void boost(void) { std::ios::sync_with_stdio(false); std::cin.tie(0); }
 
 int main (void) {
   boost();
