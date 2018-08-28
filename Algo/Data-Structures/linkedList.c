86c89bad43d74c0d8cce957bae60647b86f74594
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-29 03:05:53 +0530

    Update linkedList.c

--- Algo/Data-Structures/linkedList.c
+++ Algo/Data-Structures/linkedList.c
@@ -7,12 +7,12 @@
 typedef struct node {
     int data;
     struct node* next;
- } node;
+ } node, *ptrNode;
 
 typedef struct linkedList {
     node* head;
     node* tail;
-} linkedList, *ptr;
+} linkedList, *ptrLinkedList;
 
 linkedList*
 createNode() {
