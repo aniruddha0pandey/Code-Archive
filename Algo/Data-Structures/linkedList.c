16e58c993d2af4f4f69b7821a09c83a163863b27
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-29 01:58:12 +0530

    Update linkedList.c

--- Algo/Data-Structures/linkedList.c
+++ Algo/Data-Structures/linkedList.c
@@ -1,1 +1,33 @@
+#ifndef _LINKEDLIST_C_
+#define _LINKEDLIST_C_
 
+#include <stdio.h>
+#include <stdbool.h> // C99
+
+struct linkedList {
+    int data;
+    struct linkedList* next;
+ };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // _LINKEDLIST_C_
