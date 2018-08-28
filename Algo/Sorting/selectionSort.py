cdd9353e3f71a4473109456507537b45f4b14b22
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-29 01:09:14 +0530

    Update selectionSort.py

--- Algo/Sorting/selectionSort.py
+++ Algo/Sorting/selectionSort.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def insertionSort(list):
-    for i in range(len(list)):
+    for i in range(len(list) - 1):
         min = i
         for j in range(i + 1, len(list)):
             if list[j] < list[min]:
