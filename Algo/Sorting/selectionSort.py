077542890be052e57bd2609884ca486451a9db6c
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-28 19:20:39 +0530

    Update selectionSort.py

--- Algo/Sorting/selectionSort.py
+++ Algo/Sorting/selectionSort.py
@@ -1,7 +1,13 @@
 import numpy as np
 
 def insertionSort(list):
-    
+    for i in range(len(list) - 1):
+        min = i
+        for j in range(i + 1, len(list)):
+            if list[j] < list[min]:
+                min = j
+        list[i], list[min] = list[min], list[i]
+    return list
 
 if __name__ == "__main__":
     list = np.random.random_integers(100, size=(100, )).tolist()
