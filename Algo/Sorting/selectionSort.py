f95f18a7dcd81442aac3a82d1b797a014f3b60dc
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-28 18:41:32 +0530

    Update selectionSort.py

--- Algo/Sorting/selectionSort.py
+++ Algo/Sorting/selectionSort.py
@@ -1,1 +1,8 @@
+import random
 
+def insertionSort(list):
+    
+
+if __name__ == "__main__":
+    list = [random.randint(0, 1000) for _ in range(10)]
+    print("Sorted list: ", insertionSort(list))
