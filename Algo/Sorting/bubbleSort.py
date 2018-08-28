91308d9b13b3d841c17ba7fa80e5c709a14b5689
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-08-28 18:01:38 +0530

    Update bubbleSort.py

--- Algo/Sorting/bubbleSort.py
+++ Algo/Sorting/bubbleSort.py
@@ -1,1 +1,10 @@
+import random
 
+def bubbleSort(list):
+    
+
+if __name__ == "__main__":
+    list = [random.randint(0, 1000) for _ in range(10)]
+    print("Before Sorting  {}".format(list))
+    bubbleSort(list)
+    print("After Sorting  {}".format(list))
