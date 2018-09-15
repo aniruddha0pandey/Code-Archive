629a20819c7ab605a138485eb5e473f100548f67
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-09-16 01:24:37 +0530

    Create strlenght1.cpp

---
+++ Archive/strlenght1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+size_t strlenght (const char *str, const char *p) {
+  if (str == NULL) return 0;
+  p = str;
+  while (*p != '\0')  p++;
+  return p - str;
+}
+
+int main(void) {
+  std::string str[]("hello");
+  std::cout << strlenght(str) << std::endl;
+  return 0;
+}
