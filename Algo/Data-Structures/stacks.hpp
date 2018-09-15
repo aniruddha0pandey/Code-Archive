42f80baded4aca005f80a7839f9fa67abdb7964c
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-09-16 04:46:04 +0530

    Update stacks.hpp

--- Algo/Data-Structures/stacks.hpp
+++ Algo/Data-Structures/stacks.hpp
@@ -1,26 +1,39 @@
 // Implemented using stacks
 
 #include <iostream>
+#define SIZE 100
 
-bool push () {
-  
-}
+int stack[SIZE];
+int top = -1;
 
-bool pop () {
-  
+bool push (auto x) {
+  if (top >= SIZE) return false;
+  else {
+    stack[++top] = x;
+    return true;
+  }
 }
 
 template <class T>
-T peek () {
-  
+ T pop (void) {
+  if (top < 0) return false;
+  else stack[--top];
 }
 
-bool empty () {
-  
-  
+template <class T>
+T peek (void) {
+  return stack[top];
+}
+
+bool empty (void) {
+  return (top < 0);
 }
 
 int main (void) {
-  
+  for (int i = 0; i < 10; ++i)
+    push(i + 1);
+  std::cout << pop() << std::endl;
+  std::cout << peek() << std::endl;
+  std::cout << (empty)?"Empty":"Not Empty << std::endl;
   return 0;
 }
