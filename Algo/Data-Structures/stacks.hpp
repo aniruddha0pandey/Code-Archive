38748faf7c641ceb359f16c005b60d3af080a9f1
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-09-16 04:33:14 +0530

    Update stacks.hpp

--- Algo/Data-Structures/stacks.hpp
+++ Algo/Data-Structures/stacks.hpp
@@ -1,1 +1,26 @@
+// Implemented using stacks
 
+#include <iostream>
+
+bool push () {
+  
+}
+
+bool pop () {
+  
+}
+
+template <class T>
+T peek () {
+  
+}
+
+bool empty () {
+  
+  
+}
+
+int main (void) {
+  
+  return 0;
+}
