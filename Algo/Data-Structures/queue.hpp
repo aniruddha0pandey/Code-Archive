ee6859829be6bb6eb20c70bdf4ba167679403485
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-09-16 20:15:05 +0530

    Update and rename queue.h to queue.hpp

---
+++ Algo/Data-Structures/queue.hpp
@@ -0,0 +1,36 @@
+// Array implementation of queue
+
+#include <stdio.h>
+
+class Queue {
+private:
+  int siz;
+public:
+  Queue();
+  enQueue();
+  deQueue();
+  ~Queue();  
+}
+
+Queue::Queue(): siz(e) {
+  
+}
+
+Queue::~Queue() {
+  
+}
+
+void Queue::enQueue() {
+  
+}
+
+template <class T>
+T Queue::deQueue() {
+   
+}
+
+class status: public Queue {
+private:
+  empty();
+  size();
+}
