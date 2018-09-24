da1815d63907ebf6d57cee07bdc3482e7ac31206
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-09-25 00:19:11 +0530

    Update singlyLinkedList.c

--- Algo/Data-Structures/singlyLinkedList.c
+++ Algo/Data-Structures/singlyLinkedList.c
@@ -42,7 +42,7 @@ length (linkedList* list) {
 void
 printList (linkedList* list) {
 	if (length(list) <= 0) {
-		printf("Description : Empty List!\n");
+		printf("ERROR       : Empty List!\n");
 		return;
 	}
 	node* current = list -> head;
@@ -89,13 +89,16 @@ assignList (linkedList* list, int LB, int UB) {
 	int i;
 	for (i = UB; i > LB; )
 		if (!pushHead(list, i--))
-			printf("Memory Overflow!\n");
+			printf("ERROR       : Memory Overflow!\n");
+	for (i = UB; i > LB; )
+			if (!pushTail(list, i--))
+				printf("ERROR       : Memory Overflow!\n");
 }
 
 void*
 popHead (linkedList* list) {
 	if (length(list) <= 0) {
-		printf("Empty List!\n");
+		printf("ERROR       : Empty List!\n");
 		return;
 	}
 	node* current = list -> head;
@@ -117,163 +120,11 @@ int main (void) {
 	printList(myList);
 
 	assignList(myList, 0, 10);
-	pushTail(myList, 5);
 
 	printf("Size of List: %d\n", length(myList));
 	printList(myList);
 
 	printf("Freed Node  : 0x%p\n", popHead(myList));
-	printList(myList);
-
-	return 0;
-}
-
-		printf("%d ", current -> data);
-		current = current -> next;
-	}
-	puts("");
-}
-
-bool 
-pushHead (linkedList* list, int newData) {
-	node* newNode = getNode(newData);
-	if (newNode == NULL) return false;
-	newNode -> next = list -> head;
-	list -> head = newNode;
-	//////////// Special-Case ////////////
-	if (length(list) == 1)
-		list -> tail = newNode;
-	return true;
-}
-
-bool 
-pushTail (linkedList* list, int newData) {
-	node* newNode = getNode(newData);
-	if (newNode == NULL) return false;
-	newNode -> next = NULL;
-	//////// Determining List Tail ///////
-	node* tmp = list -> head;
-	while (tmp != NULL) {
-		list -> tail = tmp;
-		tmp = tmp -> next;
-	}
-	/////////////////////////////////////
-	node* current = list -> tail;
-	current -> next = newNode;
-	list -> tail = newNode;
-	return true;
-}
-
-void
-assignList (linkedList* list, int LB, int UB) {
-	int i;
-	for (i = UB; i > LB; )
-		if (!pushHead(list, i--))
-			printf("Memory Overflow!\n");
-}
-
-void*
-popHead (linkedList* list) {
-	if (length(list) <= 0) {
-		printf("Empty List!\n");
-		return;
-	}
-	node* current = list -> head;
-	freeNode(list -> head);
-	list -> head = current -> next;
-	current = NULL;
-}
-
-void*
-popTail () {
-
-
-}
-
-int main (void) {
-	linkedList* myList = listInit();
-
-	printf("Size of List: %d\n", length(myList));
-	printList(myList);
-
-	assignList(myList, 0, 10);
-	pushTail(myList, 5);
-
-	printf("Size of List: %d\n", length(myList));
-	printList(myList);
-
-	printf("Free Node   : %d\n", popHead(myList));
-	printList(myList);
-
-	return 0;
-}
-
-		printf("%d ", current -> data);
-		current = current -> next;
-	}
-	puts("");
-}
-
-bool 
-pushHead (linkedList* list, int newData) {
-	node* newNode = getNode(newData);
-	if (newNode == NULL) return false;
-	newNode -> next = list -> head;
-	list -> head = newNode;
-	//////////// Special-Case ////////////
-	if (length(list) == 1)
-		list -> tail = newNode;
-	return true;
-}
-
-bool 
-pushTail (linkedList* list, int newData) {
-	node* newNode = getNode(newData);
-	if (newNode == NULL) return false;
-	newNode -> next = NULL;
-	//////// Determining List Tail ///////
-	node* tmp = list -> head;
-	while (tmp != NULL) {
-		list -> tail = tmp;
-		tmp = tmp -> next;
-	}
-	/////////////////////////////////////
-	node* current = list -> tail;
-	current -> next = newNode;
-	list -> tail = newNode;
-	return true;
-}
-
-void
-assignList (linkedList* list, int LB, int UB) {
-	int i;
-	for (i = UB; i > LB; )
-		if (!pushHead(list, i--))
-			printf("Memory Overflow!\n");
-}
-
-void*
-popHead () {
-
-
-}
-
-void*
-popTail () {
-
-
-}
-
-int main (void) {
-	linkedList* myList = listInit();
-
-	printf("Size of List: %d\n", length(myList));
-	printList(myList);
-
-	assignList(myList, 0, 10);
-	pushTail(myList, 5);
-
-	printf("Size of List: %d\n", length(myList));
 	printList(myList);
 
 	return 0;
