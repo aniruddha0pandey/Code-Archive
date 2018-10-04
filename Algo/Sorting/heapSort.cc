97dea7b7373fdaabf50c8b9b25de7e98bf6a6f4e
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-10-04 21:25:45 +0530

    Update and rename heapSort to heapSort.cc

--- Algo/Sorting/heapSort.cc
+++ Algo/Sorting/heapSort.cc
@@ -1,3 +1,14 @@
 // Binary-Search Tree : Data of all the nodes in the left sub-tree of the root node should be ≤ the data of the root.
                         The data of all the nodes in the right sub-tree of the root node should be > the data of the root.
 // Binary-Heap   Tree : Data of the root node is > or < the data of two children node.
+
+#include <iostream>
+
+void maxHeap () {
+
+}
+
+int main (void) {
+
+  return 0;
+}
