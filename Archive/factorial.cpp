c18dd5097b9c04713da2316737318c054ba6a9e2
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-10-10 19:38:19 +0530

    Update factorial.cpp

--- Archive/factorial.cpp
+++ Archive/factorial.cpp
@@ -3,14 +3,14 @@
 #include <iostream>
 
 int
-fibo (int n) {
-	return (n < 1) ? 1 : n * fibo(n - 1);
+fact (int n) {
+	return (n < 1) ? 1 : n * fact(n - 1);
 }
 
 int
 main (void) {
 	int n;
 	std::cin >> n;
-	std::cout << fibo(n);
+	std::cout << fact(n);
 	return 0;
 }
