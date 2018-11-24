4417ac14ba727ac7c753bc33ac19d1db946a2222
Aniruddha Pandey <anirudh.pandev@gmail.com>
2018-11-24 18:48:03 +0530

    Update aniTemplate.h

--- Includes/aniTemplate.h
+++ Includes/aniTemplate.h
@@ -6,9 +6,17 @@
 #define sz(x) (ll)(x).size()
 
 const int MOD = 1e9 + 7;
+
 const double low = -1e10;
 const double high = 1e10;
+
+const double lowInt = INT_MIN;
+const double highInt = INT_MAX;
+
 const long long inf = (long long) 1e18;
+
+
+
 
 template <class T> inline T
 gcd (T x, T y) {
