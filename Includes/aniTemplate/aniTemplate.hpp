1f493b14e4f9c26a06ee6887134324512123ddc5
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-02-13 21:17:25 +0530

    Update aniTemplate.hpp

--- Includes/aniTemplate/aniTemplate.hpp
+++ Includes/aniTemplate/aniTemplate.hpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ull unsigned long long
+
+#define sz(x) (ll)(x).size()
+
+const int MOD = 1e9 + 7;
+
+const double low = -1e10;
+const double high = 1e10;
+
+const double lowInt = INT_MIN;
+const double highInt = INT_MAX;
+
+const long long inf = (long long) 1e18;
+
+
+
 
