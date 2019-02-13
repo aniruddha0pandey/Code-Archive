0cb23a7089edb95f0199441590ea005c2681aa82
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-02-13 21:08:13 +0530

    Update KVertexColoring.java

--- Algo/Data-Structures/Graph/KVertexColoring.java
+++ Algo/Data-Structures/Graph/KVertexColoring.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 
 public class KVertexColoring {
   public static void main(String[] args) {
-    // code
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //
   }
 }
