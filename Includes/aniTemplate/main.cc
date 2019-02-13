826ad2d322547875f74e5dc19f7668bfcfea3481
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-02-13 21:20:04 +0530

    Update main.cc

--- Includes/aniTemplate/main.cc
+++ Includes/aniTemplate/main.cc
@@ -1,1 +1,18 @@
+#include <aniTemplate.hpp>
 
+int
+main ( void ) {
+  ofstream fout( getenv("OUTPUT_PATH") );
+  boost();
+  int input, result;
+  scan( input );
+
+  int arr[] = {};
+  int n = sizeof( arr ) / sizeof( arr[0] );
+  vector<int> vect(arr, arr + n);
+
+  srand((unsigned)time(NULL));
+  fout << result << "\n";
+  fout.close();
+  return 0;
+}
