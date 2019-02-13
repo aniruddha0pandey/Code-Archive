a557bdf4e71bcc48c83ee41274a6ecb02305e2cd
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-02-13 21:29:41 +0530

    Update aniTemplate.hpp

--- Includes/aniTemplate/aniTemplate.hpp
+++ Includes/aniTemplate/aniTemplate.hpp
@@ -12,6 +12,9 @@ const int MOD = 1e9 + 7; // 1'000'000'007
 const double low = -1e10;
 const double high = 1e10;
 
+#define MAX 1e9
+#define MIN -1e9
+
 const double lowInt = INT_MIN;
 const double highInt = INT_MAX;
 
