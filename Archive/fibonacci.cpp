f40aae6b649e5902333034f9407c035d62d0ba77
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-02-17 15:44:15 +0530

    Update fibonacci.cpp

--- Archive/fibonacci.cpp
+++ Archive/fibonacci.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
-int fib ( int n ) {
+int fibo( int n ) {
   return (n <= 1) ? 1 : fibo(n - 1) + fibo(n - 2);
 }
 
 int main ( void ) {
-  
+	int n;
+	std::cin >> n;
+  std::cout << fibo(n);
   return 0;
 }
   
