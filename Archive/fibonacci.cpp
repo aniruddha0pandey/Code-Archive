5ac07e7285a763cb468baf10f045008f9762e0fc
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-02-20 02:13:40 +0530

    Update fibonacci.cpp

--- Archive/fibonacci.cpp
+++ Archive/fibonacci.cpp
@@ -2,7 +2,7 @@
 
 int memoized_fibo( int n ) {
 	int* result = new int[ n ];
-	result[0] = result[1] = 1;
+	result[ 0 ] = result[ 1 ] = 1;
 	for (int i = 2; i < n; ++i)
 		result[i] = result[i - 1] + result[i - 2];
 	return result[n - 1];
