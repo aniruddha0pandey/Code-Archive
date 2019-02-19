c44bd0fc036ca3ff037c43251c11c360a07d287a
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-02-20 01:35:30 +0530

    Update fibonacci.cpp

--- Archive/fibonacci.cpp
+++ Archive/fibonacci.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+int memoized_fibo( int n ) {
+	result[0] = result[1] = 1;
+	for (int i = 2; i < n; ++i)
+		result[i] = result[i - 1] + result[i - 2];
+}
+
 int fibo( int n ) {
 	return (n <= 1) ? 1 : fibo(n - 1) + fibo(n - 2);
 }
@@ -7,7 +13,10 @@ int fibo( int n ) {
 int main ( void ) {
 	int n;
 	std::cin >> n;
-	std::cout << fibo(n);
+	int* result = new int[ n ];
+	std::cout << fibo( n );
+	memoized_fibo( n );
+	std::cout << result[n];
 	return 0;
 }
   
