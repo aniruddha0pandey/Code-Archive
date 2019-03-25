eca624edf4db6d4c4f0e0927301e8d182c52afac
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-03-25 18:49:32 +0530

    Update main.py

--- Author/Syntax_Mafia/main.py
+++ Author/Syntax_Mafia/main.py
@@ -56,14 +56,17 @@ def limitfy(words, line_limit, para_line = [], lines = (), length = 0, i = 0):
 
 
 if __name__ == '__main__':
-	# try: test_cases, line_limit = int(input()), int(input())
-	# except ValueError: test_cases, line_limit = 1, 30 # 14(min)
-	# string = input() or "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quid nunc honeste dicit? Huc et illuc, Torquate, vos versetis licet, nihil in hac praeclara epistula scriptum ab Epicuro congruens et conveniens decretis eius reperietis. Roges enim Aristonem, bonane ei videantur haec: vacuitas doloris, divitiae, valitudo; Ergo instituto veterum, quo etiam Stoici utuntur, hinc capiamus exordium. Aliter homines, aliter philosophos loqui putas oportere? Sed utrum hortandus es nobis, Luci, inquit, an etiam tua sponte propensus es? Duo Reges: constructio interrete. Scripta sane et multa et polita, sed nescio quo pacto auctoritatem oratio non habet. Alii rursum isdem a principiis omne officium referent aut ad voluptatem aut ad non dolendum aut ad prima illa secundum naturam optinenda. Non pugnem cum homine, cur tantum habeat in natura boni; Summus dolor plures dies manere non potest? Quamquam scripsit artem rhetoricam Cleanthes, Chrysippus etiam, sed sic, ut, si quis obmutescere concupierit, nihil aliud legere debeat. Habes, inquam, Cato, formam eorum, de quibus loquor, philosophorum. Id enim volumus, id contendimus, ut officii fructus sit ipsum officium."
-
 	file = open("./tests/test1.in", "r")
-	test_cases = int(file.readline())
-	line_limit = int(file.readline())
-	string = file.readline()
+
+	try:
+		test_cases, line_limit = int(file.readline()), int(file.readline())
+		string = file.readline()
+	except IOError:
+		test_cases, line_limit = int(input()), int(input())
+		string = input()
+	except ValueError:
+		test_cases, line_limit = 1, 30 # 14(min)
+		string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quid nunc honeste dicit? Huc et illuc, Torquate, vos versetis licet, nihil in hac praeclara epistula scriptum ab Epicuro congruens et conveniens decretis eius reperietis. Roges enim Aristonem, bonane ei videantur haec: vacuitas doloris, divitiae, valitudo; Ergo instituto veterum, quo etiam Stoici utuntur, hinc capiamus exordium. Aliter homines, aliter philosophos loqui putas oportere? Sed utrum hortandus es nobis, Luci, inquit, an etiam tua sponte propensus es? Duo Reges: constructio interrete. Scripta sane et multa et polita, sed nescio quo pacto auctoritatem oratio non habet. Alii rursum isdem a principiis omne officium referent aut ad voluptatem aut ad non dolendum aut ad prima illa secundum naturam optinenda. Non pugnem cum homine, cur tantum habeat in natura boni; Summus dolor plures dies manere non potest? Quamquam scripsit artem rhetoricam Cleanthes, Chrysippus etiam, sed sic, ut, si quis obmutescere concupierit, nihil aliud legere debeat. Habes, inquam, Cato, formam eorum, de quibus loquor, philosophorum. Id enim volumus, id contendimus, ut officii fructus sit ipsum officium."
 
 	file = open("./tests/test1.out", "w")
 
