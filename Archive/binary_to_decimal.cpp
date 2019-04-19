d3832df09cd83e2ff3d29943d9bf48a3037c541c
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-04-20 00:03:24 +0530

    Update binary_to_decimal.cpp

--- Archive/binary_to_decimal.cpp
+++ Archive/binary_to_decimal.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int main (void) {
-    int tmp = 0;
     std::string S;
     std::cin >> S;
     
+    int tmp = 0;
     for (int i = 0; i < S.length(); i++)
         tmp += int(S[S.length() - i - 1] - '0') * (1 << i);
     
