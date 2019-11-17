d27476f0719ea325fc00abf99c7bbeca4d7c106e
pandevim <anirudh.pandev@gmail.com>
2019-11-17 16:23:56 +0530

    update

--- Author/No_Nut_November/build.md
+++ Author/No_Nut_November/build.md
@@ -1,4 +1,5 @@
 ```
 $ cd tests
 $ for i in $(seq 0 3); do mkdir $i && cd ./$i && touch in out && cd ..; done
+$ make main && ./main < tests/0/in > tests/0/out
 ```
--- Author/No_Nut_November/main.cc
+++ Author/No_Nut_November/main.cc
@@ -1,9 +1,47 @@
 #include <bits/stdc++.h>
 
+template<typename Arg> inline void 
+increment ( Arg& arg ) {
+	arg = arg + 1;
+}
+
+template<typename Arg, typename ... Args> inline void 
+increment ( Arg& arg, Args& ... args ) {
+	arg = arg + 1;
+	increment(args...);
+}
+
+int 
+max_nuts( std::vector<char> v, int k ) {
+	int max;
+	std::vector<int> squirrels, nuts;
+
+	for ( int i = 0; i < v.size(); ++i ) {
+		if ( v[i] == 'S' ) squirrels.push_back(i);
+		else if ( v[i] == 'N' ) nuts.push_back(i);
+	}
+
+	int i = 0, j = 0;
+	while ( i < nuts.size() && j < squirrels.size() ) {
+		if ( std::abs(nuts[i] - squirrels[j]) <= k )
+			increment<int, int, int>(max, i, j);
+		else if ( nuts[i] < squirrels[j] )
+			increment<int>(i);
+		else
+			increment<int>(j);
+	}
+  
+	return max;
+}
+
 int 
 main ( void ) {
-	
-
-
+	int t; std::cin >> t;
+	while ( t-- ) {
+		std::vector<char> v;
+		int k, n; std::cin >> k >> n;
+		std::copy_n(std::istream_iterator<char>(std::cin), n, std::back_inserter(v));
+		std::cout << max_nuts(v, k) << std::endl;
+	}
 	return 0;
 }
